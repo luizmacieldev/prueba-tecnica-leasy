4b2bf7bbbfd629b59c847931c8d3ad2c8776ea18
luizlgam <lgamjoeperry@gmail.com>
2026-05-18 21:16:09 -0300

    Added client reservation cancellation domain errors

--- src/client_portal/exceptions.py
+++ src/client_portal/exceptions.py
@@ -1,4 +1,4 @@
-from shared.errors import AuthError
+from shared.errors import AuthError, AppError
 
 
 class InvalidCredentialsError(AuthError):
@@ -9,3 +9,18 @@ class InvalidCredentialsError(AuthError):
 class InvalidTokenError(AuthError):
     code = "auth_invalid_token"
     detail = "Invalid access token."
+
+
+class ReservationCancellationNotAllowedError(AppError):
+    code = "reservation_cancellation_not_allowed"
+    detail = "Reservation cannot be cancelled."
+
+
+class ReservationCancellationReasonRequiredError(AppError):
+    code = "reservation_cancellation_reason_required"
+    detail = "Cancellation reason is required."
+
+
+class ReservationOwnershipError(AppError):
+    code = "reservation_forbidden"
+    detail = "You cannot cancel this reservation."
